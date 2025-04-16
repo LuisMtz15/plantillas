0aea41d12be950ba09fb3b66ebadc50bcd3143de
Its-Yayo <yayitzz@proton.me>
2025-04-16 01:28:31 -0600

    BloqueInformacion componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import React from "react";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import BloqueInformacion from "./components/BloqueInformacion";
 
 function App() {
   return (
     <div>
       <Header />
-      <br />
+      <BloqueInformacion />
       <Footer />
     </div>
   );
